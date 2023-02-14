8842649fae0325ed306f79cde4a6375d0d861ac6
timmy338 <66931238+timmy338@users.noreply.github.com>
2023-02-14 17:03:55 +0800

    create navi
    在首頁加了導航分類,頂部半圓樣式

--- main.js
+++ main.js
@@ -4,17 +4,26 @@ import App from './App'
 
 //導入網絡請求的包,掛載到Uniapp
 import { $http } from '@escook/request-miniprogram'
-uni.$http=$http
-$https.baseUrl=' https://www.esinsis.tech'
+uni.$http = $http
+// 配置请求根路径
+$http.baseUrl = 'https://www.esinsis.tech'
+
 // 请求开始之前做一些事情
 $http.beforeRequest = function (options) {
   uni.showLoading({
     title: '数据加载中...',
   })
 }
+
 // 请求完成之后做一些事情
 $http.afterRequest = function () {
   uni.hideLoading()
+}
+uni.$showMsg=function(title='數據請求失敗!',duration=1500){
+  uni.showToast({
+    title,
+    duration
+  })
 }
 
 Vue.config.productionTip = false
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,6 +1,24 @@
 <template>
   <view>
-    Home
+    <div class="box1">
+      <div class="box"></div>
+    </div>
+
+    <!--轮播图-->
+    <swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+      <swiper-item v-for="(item,i) in swiperList" :key="i">
+        <navigator class="swiper-item" :url="'/subpkg/goods_detail/goods_detail?good_id='+item.goods_id">
+          <image :src="item.image_src"></image>
+        </navigator>
+      </swiper-item>
+    </swiper>
+
+    <!-- 導航 -->
+    <view class="nav-list">
+      <view class="nav-item" v-for="(item,i) in navList" :key="i">
+        <image class="nav-img" :src="item.image_src" />
+      </view>
+    </view>
   </view>
 </template>
 
@@ -8,21 +26,67 @@
   export default {
     data() {
       return {
-        swiperList:[],
+        swiperList: [],
+        navList: []
       };
     },
-    onLoad(){
-      this.getSwiperList()
+    onLoad() {
+      this.getSwiperList(),
+        this.getNavList()
     },
-    methods:{
-      async getSwiperList(){
-        const res=uni.$http.get('/api/public/v1/home/swiperdata')
-        console.log(res)
+    methods: {
+      //輪播圖數據
+      async getSwiperList() {
+        const {
+          data: res
+        } = await uni.$http.get('/api/public/v1/home/swiperdata')
+        if (res.meta.status !== 200) return uni.$showMsg()
+        this.swiperList = res.message
+        /* console.log(res) */
+      },
+      //导航數據
+      async getNavList() {
+        const {
+          data: res
+        } = await uni.$http.get('/api/public/v1/home/catitems')
+        if (res.meta.status !== 200) return uni.$showMsg()
+        this.navList = res.message
+        /* console.log(res) */
       }
     }
   }
 </script>
 
 <style lang="scss">
+  .box1 .box {
+    position: absolute;
+    left: -30%;
+    width: 150%;
+    height: 150px;
+    background-image: linear-gradient(0deg, #b1f0ec, #1D9BF0 50%);
+    border-bottom-left-radius: 40%;
+    border-bottom-right-radius: 100%;
+  }
 
+  swiper {
+    height: 330rpx;
+    margin: 0 20rpx;
+
+    .swiper-item,
+    image {
+      width: 100%;
+      height: 100%;
+    }
+  }
+
+  .nav-list {
+    display: flex;
+    justify-content: space-around;
+    margin: 15rpx 0;
+
+    .nav-img {
+      width: 128rpx;
+      height: 140rpx;
+    }
+  }
 </style>
