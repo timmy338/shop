6224e93695bde5ddbb4f75a545e749f2ac87da39
timmy338 <66931238+timmy338@users.noreply.github.com>
2023-02-20 15:03:52 +0800

    init goods_list
    完成商品列表頁的初步設計

--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -3,10 +3,10 @@
   <view style="background-color: #F4F4F4;">
     <!-- 商品列表 -->
     <view class="goods-list">
-          <template v-for="(item, i) in goodsList">
+          <view class="goods-box" v-for="(item, i) in goodsList" @click="gotoDetail(item)">
             <!-- 为 my-goods 组件动态绑定 goods 属性的值 -->
             <my-goods :goods="item"></my-goods>
-          </template>
+          </view>
     </view>
   </view>
 </template>
@@ -26,6 +26,7 @@
         },
         goodsList: [],
         total: 0,
+        isloading:false
       };
     },
     onLoad(options) {
@@ -36,17 +37,43 @@
     },
     methods: {
       // 获取商品列表数据的方法
-      async getGoodsList() {
+      async getGoodsList(cb) {
+        this.isloading=true
         // 发起请求
         const {
           data: res
         } = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+        this.isloading=false
+        cb && cb()
         if (res.meta.status !== 200) return uni.$showMsg()
         // 为数据赋值
-        this.goodsList = res.message.goods
+        this.goodsList = [...this.goodsList,...res.message.goods]
         this.total = res.message.total
-      }
-    }
+      },
+      gotoDetail(goods){
+        uni.navigateTo({
+          url:'/subpkg/goods_detail/goods_detail?goods_id='+goods.goods_id
+        })
+      },
+    },
+    onReachBottom(){
+      //判斷是否還有下一頁數據
+      if(this.queryObj.pagenum*this.queryObj.pagesize>=this.total){
+        return uni.$showMsg('數據加載完畢')
+      } 
+      if(this.isloading) return
+      //頁碼++
+      this.queryObj.pagenum++
+      this.getGoodsList()
+    },
+    onPullDownRefresh(){
+      this.queryObj.pagenum=1
+      this.total=0
+      this.isloading=false
+      this.goodsList=[]
+      this.getGoodsList(()=>uni.stopPullDownRefresh())
+    },
+
   }
 </script>
 
@@ -56,5 +83,8 @@
     display: flex;
     flex-wrap: wrap;
     justify-content: space-around;
+    .goods-box{
+      width: 48%;
+    }
   }
 </style>
