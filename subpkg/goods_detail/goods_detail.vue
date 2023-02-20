c54676cbf3bf74b9f782ec8f96a86c058bbad321
timmy338 <66931238+timmy338@users.noreply.github.com>
2023-02-20 16:27:24 +0800

    init goods_detail page
    完成商品詳情頁的初稿

--- subpkg/goods_detail/goods_detail.vue
+++ subpkg/goods_detail/goods_detail.vue
@@ -1,6 +1,41 @@
 <template>
-  <view>
-    goods_deatil_page
+  <view class="goods-detail-container" v-if="goods_info.goods_name">
+    <!-- 轮播图区域 -->
+    <swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+      <swiper-item v-for="(item, i) in goods_info.pics" :key="i">
+        <image :src="item.pics_big" @click="preview(i)"></image>
+      </swiper-item>
+    </swiper>
+    <!-- 商品信息区域 -->
+    <view class="goods-info-box">
+      <!-- 商品价格 -->
+      <view class="price">
+        ￥{{goods_info.goods_price}}
+      </view>
+      <!-- 信息主体区域 -->
+      <view class="goods-info-body">
+        <view class="goods-name">
+          {{goods_info.goods_name}}
+        </view>
+        <!-- 收藏 -->
+        <view class="favi">
+          <uni-icons type="star" size="18" color="gray"></uni-icons>
+          <text>收藏</text>
+        </view>
+      </view>
+      <view v-for="i in 20 " style="height: 20px;background-color: antiquewhite;"> test</view>
+    </view>
+
+    <!-- 商品导航组件 -->
+    <view class="goods_nav">
+      <!-- fill 控制右侧按钮的样式 -->
+      <!-- options 左侧按钮的配置项 -->
+      <!-- buttonGroup 右侧按钮的配置项 -->
+      <!-- click 左侧按钮的点击事件处理函数 -->
+      <!-- buttonClick 右侧按钮的点击事件处理函数 -->
+      <uni-goods-nav :fill="true" :options="options" :buttonGroup="buttonGroup" @click="onClick"
+        @buttonClick="buttonClick" />
+    </view>
   </view>
 </template>
 
@@ -8,12 +43,122 @@
   export default {
     data() {
       return {
-        
+        goods_info: {},
+        // 左侧按钮组的配置对象
+        options: [{
+          icon: 'star',
+          text: '收藏'
+        }, {
+          icon: 'cart',
+          text: '购物车',
+          info: 2
+        }],
+        // 右侧按钮组的配置对象
+        buttonGroup: [{
+            text: '加入购物车',
+            backgroundColor: 'linear-gradient(90deg, #1D9BF0, #1c63f0)',
+            color: '#fff'
+          },
+          {
+            text: '立即购买',
+            backgroundColor: 'linear-gradient(90deg, #60F3FF, #088FEB)',
+            color: '#fff'
+          }
+        ]
       };
+    },
+    onLoad(options) {
+      const goods_id = options.goods_id
+      this.getGoodsDetail(goods_id)
+    },
+    methods: {
+      // 获取商品数据的方法
+      async getGoodsDetail(goods_id) {
+        const {
+          data: res
+        } = await uni.$http.get('/api/public/v1/goods/detail', {
+          goods_id
+        })
+        if (res.meta.status !== 200) return uni.$showMsg()
+        // 为数据赋值
+        this.goods_info = res.message
+      },
+      // 实现轮播图的预览效果
+      preview(i) {
+        // 调用 uni.previewImage() 方法预览图片
+        uni.previewImage({
+          // 预览时，默认显示图片的索引
+          current: i,
+          // 所有图片 url 地址的数组
+          urls: this.goods_info.pics.map(x => x.pics_big)
+        })
+      },
+      onClick(e) {
+        if (e.content.text === '购物车') {
+          // 切换到购物车页面
+          uni.switchTab({
+            url: '/pages/cart/cart'
+          })
+        }
+      }
     }
   }
 </script>
 
 <style lang="scss">
+  .goods-detail-container {
+    // 给页面外层的容器，添加 50px 的内padding，
+    // 防止页面内容被底部的商品导航组件遮盖
+    padding-bottom: 50px;
+  }
 
+  swiper {
+    height: 750rpx;
+
+    image {
+      width: 100%;
+      height: 100%;
+    }
+  }
+
+  .goods-info-box {
+    padding: 10px;
+
+    .price {
+      margin: 10px 0;
+      font-size: 18px;
+      font-weight: bold;
+      color: #EB554E;
+    }
+
+    .goods-info-body {
+      display: flex;
+      justify-content: space-between;
+
+      .goods-name {
+        font-size: 14px;
+        font-weight: bold;
+        padding-right: 10px;
+      }
+
+      .favi {
+        width: 120px;
+        font-size: 12px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        border-left: 1px solid #efefef;
+        color: gray;
+      }
+    }
+  }
+
+  .goods_nav {
+    // 为商品导航组件添加固定定位
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+  }
 </style>
